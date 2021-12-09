ae0c3d1a37c122e70a7627d4e099bc23ed0df320
RedWn <redwaniscool@gmail.com>
2021-12-09 20:30:37 +0200

    Added All products page
    a prototype for a main page to hold all the products in the database audience

--- lib/productspage.dart
+++ lib/productspage.dart
@@ -1,30 +1,48 @@
 import 'package:test4/main.dart';
 import 'package:flutter/material.dart';
 
+class Controller {
+  static int state = 0;
+  static void update(int newState) {
+    state = newState;
+    if (newState == 1) {
+      Product.X = 2;
+    }
+  }
+}
+
 class Product {
-  static Widget get (BuildContext context){
+  static int X = 1;
+  static Widget get(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 50,vertical: 20),
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        color: Assets.backgroundColor.withOpacity(0.9),
-          borderRadius: BorderRadius.all(
-              Radius.circular(Assets.roundCorners)),
-          boxShadow: [
-            BoxShadow(
-              offset: const Offset(2, 2),
-              blurRadius: 6,
-              spreadRadius: 1,
-              color: Assets.shadowColor,
-            )
-          ]),
-      child: Column(
-        children: [
-          Image.asset('Assets/test1.png'),
-          Text('Product Name', style: TextStyle(fontSize: 20, fontFamily: Assets.mainFont, color: Assets.textColor, height: 2),),
-            ]
-          )
-      );
+        margin: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            color: Assets.backgroundColor.withOpacity(0.9),
+            borderRadius:
+                BorderRadius.all(Radius.circular(Assets.roundCorners)),
+            boxShadow: [
+              BoxShadow(
+                offset: const Offset(2, 2),
+                blurRadius: 6,
+                spreadRadius: 1,
+                color: Assets.shadowColor,
+              )
+            ]),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Image.asset(
+            'Assets/test$X.png',
+            fit: BoxFit.fill,
+          ),
+          Text(
+            'Very Cool Red Car',
+            style: TextStyle(
+                fontSize: 20,
+                fontFamily: Assets.mainFont,
+                color: Assets.textColor,
+                height: 2),
+          ),
+        ]));
   }
 }
 
@@ -38,14 +56,43 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
+    var indexer = 0;
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Assets.backgroundColor,
-        drawer: Drawer(
-
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Assets.primaryColor.withOpacity(0.9),
+          foregroundColor: Assets.backgroundColor,
+          child: Icon(Icons.add_circle_rounded),
+          onPressed: () {},
         ),
+        appBar: AppBar(
+          foregroundColor: Assets.backgroundColor,
+          title: Text('All Products'),
+          backgroundColor: Assets.primaryColor.withOpacity(0.9),
+        ),
+        backgroundColor: Assets.backgroundColor,
+        bottomNavigationBar: BottomNavigationBar(
+            fixedColor: Assets.primaryColor,
+            currentIndex: indexer,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.featured_play_list), label: "All Products"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.featured_play_list), label: "My Products"),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite),
+                label: "favorites",
+              )
+            ],
+            onTap: (index) {
+              setState(() {
+                indexer = index;
+                Product.X = index + 1;
+              });
+            }),
         body: ListView(
           children: [
+            Product.get(context),
             Product.get(context),
             Product.get(context),
             Product.get(context),
